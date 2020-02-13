90b85e20f4e12232ad2e075dc98edfacb2c0f102
Jerry <jerrysizhang@gmail.com>
2020-02-12 20:33:22 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module cc.jerry.local {
+	exports cc.jerry.local;
 	exports cc.jerry.local.gui.export;
 	exports cc.jerry.local.gui;
 	exports cc.jerry.local.gui.popups;
 	exports cc.jerry.local.utils;
-	exports cc.jerry.local.main;
 	exports cc.jerry.local.gui.editor;
 
-	requires Jerry.s.Java.Commons;
 	requires centerdevice.nsmenufx;
 	requires java.desktop;
 	requires javafx.base;
@@ -15,4 +14,5 @@ module cc.jerry.local {
 	requires org.json;
 	requires org.apache.commons.io;
 	requires org.apache.commons.lang3;
+	requires cc.jerry.commons;
 }
